8e2e76a0b95c3274913e6ff7c4cbe1108f5e87f0
Renko_sky <philsky28@gmail.com>
2024-05-17 02:09:37 +0800

    wip:修改移动路径

--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -1,34 +1,68 @@
 'use client';
 import React, { useEffect, useRef } from 'react';
-import GameCanvas from './game';
+import GameCanvas from '@/app/journey/game.ts';
 import tempale1 from '@/../public/tempale1.png';
 import tempale2 from '@/../public/tempale2.png';
-
 import './styles.scss';
+import user from '@/../public/man.png';
 import { cn } from '@/lib/utils';
-
 //向右x+50y+14;
 //向左x - 48 y-22;
-type Point = { x: number; y: number };
+type Point = { x: number; y: number; center: { x: number; y: number } };
 type Routes = Point[];
+// 180 ,90
+// 235,108
+const centers = [
+	{ x: 180, y: 104 },
+	{ x: 230, y: 120 },
+	{ x: 282, y: 130 },
+	{ x: 340, y: 145 },
+	{ x: 295, y: 165 },
+	{ x: 250, y: 185 },
+	{ x: 200, y: 209 },
+	{ x: 150, y: 230 },
+	{ x: 105, y: 250 },
+	{ x: 150, y: 265 },
+	{ x: 205, y: 276 },
+	{ x: 165, y: 300 }
+];
 
-function right(point: { x: number; y: number }) {
-	return { x: point.x + 53, y: point.y + 12 };
+function right(point: Point) {
+	return {
+		x: point.x + 53,
+		y: point.y + 12
+		// center: { x: point.center.x + 55, y: point.center.y + 15 }
+	};
 }
-function left(point: { x: number; y: number }) {
-	return { x: point.x - 48, y: point.y + 22 };
+function left(point: Point) {
+	return {
+		x: point.x - 48,
+		y: point.y + 22
+		// center: { x: point.center.x - 55, y: point.center.y + 25 }
+	};
 }
-function up(point: { x: number; y: number }) {
-	return { x: point.x + 47, y: point.y - 22 };
+function up(point: Point) {
+	return {
+		x: point.x + 48,
+		y: point.y - 22
+		// center: { x: point.center.x + 47, y: point.center.y - 22 }
+	};
 }
-function upLeft(point: { x: number; y: number }) {
-	return { x: point.x - 52, y: point.y - 12 };
+function upLeft(point: Point) {
+	return {
+		x: point.x - 53,
+		y: point.y - 12
+		// center: { x: point.center.x - 52 / 2, y: point.center.y - 12 }
+	};
 }
 export default function Game() {
 	const canvasRef = useRef(null);
 	const animateCanvasRef = useRef(null);
-	function genterateRoutes(
-		start: { x: number; y: number },
+	const gameRef = useRef(null);
+	const gridcanvasRef = useRef(null);
+	const cordinateCanvasRef = useRef(null);
+	function generateRoutes(
+		start: { x: number; y: number; center: { x: number; y: number } },
 		steps: { dir: 'right' | 'left' | 'up' | 'upLeft'; times: number }[]
 	) {
 		const routes = [start];
@@ -66,6 +100,7 @@ export default function Game() {
 		console.log(routes, 'routes');
 		return routes;
 	}
+
 	function drawBoard(routes: Routes) {
 		const context = canvasRef.current?.getContext('2d');
 		context.fillStyle = 'rgb(250,229,208)'; // 设置方块的颜色
@@ -76,68 +111,79 @@ export default function Game() {
 			context.rotate(-Math.PI * 0.2); // 旋转45度
 			context.transform(1, 0.2, 0.9, 1, 0, 0);
 			context.scale(1, 0.8); // 缩放y轴，使得形状变为菱形
-			context.fillRect(-35, -35, 50, 50); // 绘制方块
+			context.fillRect(-35, -35, 48, 48); // 绘制方块
 			context.restore(); // 恢复原有的绘图状态
 		});
 	}
+
 	useEffect(() => {
-		const routes = genterateRoutes({ x: 220, y: 128 }, [
-			{ dir: 'right', times: 3 },
-			{ dir: 'left', times: 5 },
-			{ dir: 'right', times: 2 },
-			{ dir: 'left', times: 3 },
-			{ dir: 'right', times: 3 },
-			{ dir: 'left', times: 2 },
-			{ dir: 'right', times: 2 },
-			{ dir: 'up', times: 4 },
-			{ dir: 'right', times: 4 },
-			{ dir: 'up', times: 4 },
-			{ dir: 'upLeft', times: 4 },
-			{ dir: 'up', times: 5 },
-			{ dir: 'upLeft', times: 10 },
-			{ dir: 'left', times: 2 }
-		]);
+		const routes = generateRoutes(
+			{ x: 220, y: 128, center: { x: 180, y: 105 } },
+			[
+				{ dir: 'right', times: 3 },
+				{ dir: 'left', times: 5 },
+				{ dir: 'right', times: 2 },
+				{ dir: 'left', times: 3 },
+				{ dir: 'right', times: 3 },
+				{ dir: 'left', times: 2 },
+				{ dir: 'right', times: 2 },
+				{ dir: 'up', times: 4 },
+				{ dir: 'right', times: 4 },
+				{ dir: 'up', times: 4 },
+				{ dir: 'upLeft', times: 4 },
+				{ dir: 'up', times: 4 },
+				{ dir: 'upLeft', times: 10 },
+				{ dir: 'left', times: 1 }
+			]
+		)?.map((route, inx) => ({ ...route, center: centers[inx] }));
+		console.log(routes, 'routes');
 		let game = new GameCanvas({
 			id: 'anicanvas',
 			width: window.innerWidth * 0.7,
-			height: 750,
+			height: window.innerHeight,
 			routes: routes,
 			passRoutes: [],
-			manPic: tempale1.src
+			manPic: user.src
 		});
 		let bg = new GameCanvas({
 			id: 'canvas',
 			width: window.innerWidth * 0.7,
-			height: 750,
+			height: window.innerHeight,
 			routes: [],
 			passRoutes: []
 		});
 
-		async function go() {
-			await game.animate(1);
-		}
+		gameRef.current = game;
 		console.log(canvasRef.current, 'canvasRef.current');
 		if (animateCanvasRef.current) {
-			animateCanvasRef.current?.addEventListener('click', go);
+			// animateCanvasRef.current?.addEventListener('click', go);
 		}
 		if (canvasRef.current) {
 			const img = new Image();
 			img.onload = function () {
 				const context = canvasRef.current.getContext('2d');
-				context?.drawImage(img, 25, 20, 179 / 2, 160 / 2);
-				const animaContext = animateCanvasRef?.current?.getContext('2d');
-				animaContext?.drawImage(img, 200, 100, 179 / 2, 160 / 2);
+				context?.drawImage(img, 130, 150, 179 / 2, 160 / 2);
 			};
+
 			img.src = tempale1.src;
 			const img2 = new Image();
 			img2.onload = function () {
 				const context = canvasRef.current.getContext('2d');
-				context?.drawImage(img2, 100, 450, 179 / 2, 160 / 2);
+
+				context?.drawImage(img2, 80, 380, 179 / 2, 160 / 2);
 			};
 			img2.src = tempale2.src;
 			drawBoard(routes);
 		}
-
+		if (animateCanvasRef?.current) {
+			const userIcon = new Image();
+			userIcon.onload = function () {
+				const animaContext = animateCanvasRef?.current?.getContext('2d');
+				console.log(animaContext, 'animate', animateCanvasRef);
+				animaContext?.drawImage(userIcon, 180, 105, 30, 30);
+			};
+			userIcon.src = user.src;
+		}
 		return () => {
 			if (canvasRef.current) {
 				animateCanvasRef.current?.removeEventListener('click', go);
@@ -145,23 +191,92 @@ export default function Game() {
 		};
 	}, []);
 
-	return (
-		<div className="h-screen w-screen  overflow-hidden bg-[#111827]">
-			<div className="box h-full w-full">
-				<canvas id="canvas" ref={canvasRef} className=" w-screen"></canvas>
-				<canvas
-					id="anicanvas"
-					ref={animateCanvasRef}
-					style={{ position: 'absolute', zIndex: 1, top: 0, left: 0, right: 0 }}
-					className="h-screen w-screen"
-				></canvas>
-				{/* <button
-					style={{ zIndex: 6 }}
-					className="btn z-6 absolute left-1/2 top-1/2 h-16 w-16 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-gradient-to-r from-blue-500 to-purple-600 px-1 py-1 text-xs font-bold text-white shadow-md transition duration-300 ease-in-out hover:from-purple-500 hover:to-blue-600 hover:shadow-lg"
-				>
-					roll the dice
-				</button> */}
+	useEffect(() => {
+		const canvas = gridcanvasRef.current;
+		const context = canvas.getContext('2d');
+		const width = canvas.width;
+		const height = canvas.height;
+		const gridSpacing = 15;
+		const cordinate = cordinateCanvasRef.current;
+		const cordinateContext = cordinate?.getContext('2d');
+		// Draw grid lines
+		for (let x = 0; x <= width; x += gridSpacing) {
+			context.moveTo(x, 0);
+			context.lineTo(x, height);
+		}
 
+		for (let y = 0; y <= height; y += gridSpacing) {
+			context.moveTo(0, y);
+			context.lineTo(width, y);
+		}
+
+		context.strokeStyle = '#ddd';
+		context.stroke();
+
+		// Add mouse move event listener to show coordinates
+		cordinate.addEventListener('mousemove', (event) => {
+			const rect = cordinate.getBoundingClientRect();
+			const x = event.clientX - rect.left;
+			const y = event.clientY - rect.top;
+			cordinateContext.save(); // Save current state of the canvas
+			cordinateContext.clearRect(0, 0, width, height); // Clear previous drawings
+			cordinateContext.fillText(`x: ${x}, y: ${y}`, x, y); // Draw coordinates
+			cordinateContext.restore(); // Restore the state of the canvas
+		});
+	}, []);
+
+	async function go() {
+		await gameRef.current.animate(1);
+	}
+	return (
+		<div className="h-full overflow-auto ">
+			<div className="box flex">
+				<div>
+					<canvas
+						style={{ marginTop: 10 }}
+						id="canvas"
+						ref={canvasRef}
+						width={window.innerWidth * 0.7}
+						height={window.innerHeight}
+					></canvas>
+					<canvas
+						id="anicanvas"
+						ref={animateCanvasRef}
+						style={{
+							position: 'absolute',
+							zIndex: 5,
+							top: 10,
+							left: 0,
+							right: 0
+						}}
+						width={window.innerWidth * 0.7}
+						height={window.innerHeight}
+					></canvas>
+					<canvas
+						ref={gridcanvasRef}
+						width={window.innerWidth * 0.7}
+						height={window.innerHeight}
+						style={{
+							position: 'absolute',
+							zIndex: 50,
+							top: 10,
+							left: 0,
+							right: 0
+						}}
+					/>
+					<canvas
+						ref={cordinateCanvasRef}
+						width={window.innerWidth * 0.7}
+						height={window.innerHeight}
+						style={{
+							position: 'absolute',
+							zIndex: 55,
+							top: 10,
+							left: 0,
+							right: 0
+						}}
+					/>
+				</div>
 				<div
 					style={{ zIndex: 6 }}
 					className={cn(
@@ -179,6 +294,13 @@ export default function Game() {
 						</div>
 					</div>
 				</div>
+				<button
+					className="btn"
+					style={{ marginLeft: 30, fontSize: 40 }}
+					onClick={() => go()}
+				>
+					Go
+				</button>
 			</div>
 		</div>
 	);
