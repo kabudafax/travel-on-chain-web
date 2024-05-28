3e89f56fe2463555842b609035bd860334a57f72
kabudafax <lewisnie123@163.com>
2024-05-28 17:46:45 +0800

    feat: add chatbot for travel plans

--- src/app/journey/layout.tsx
+++ src/app/journey/layout.tsx
@@ -1,9 +1,11 @@
+import Chat from '@/components/chat-bot/chat';
 import { Navbar } from '@/components/navbar';
 
 const GameLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div>
 			<Navbar className="bg-[#111827]" />
+			<Chat />
 			{children}
 		</div>
 	);
